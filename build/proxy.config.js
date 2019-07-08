7813fb8eef30cba6caeb1ed54500f83f2705070c
lirifa <44889418+lirifa@users.noreply.github.com>
2019-07-08 14:47:40 +0800

    Update proxy.config.js

--- build/proxy.config.js
+++ build/proxy.config.js
@@ -2,7 +2,7 @@ module.exports = {
   proxyList: {
     '/yapi': {
       // yapi测试环境
-      target: 'http://yapi.dev.yitaichang.cn/mock/23',  // 接口域名
+      target: 'http://192.168.1.95:8082',  // 接口域名
       changeOrigin: true,  //是否跨域
       pathRewrite: {
         '^/yapi': '/',
@@ -18,8 +18,8 @@ module.exports = {
     },
     '/': {
       // 开发环境
-      // target: 'http://platform-admin.test.yitaichang.cn',  // test接口域名
-      target: 'http://consumer-h5-app.dev.yitaichang.cn',  // dev接口域名
+      // target: 'http://192.168.1.95:8082',  // test接口域名
+      target: 'http://192.168.1.95:8082',  // dev接口域名
       changeOrigin: true,  //是否跨域
     },
   }
